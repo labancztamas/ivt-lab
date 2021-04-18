4cd0146bb679638ec9f067a366247293930ae99a
labancztamas <labancz.tamas@gmail.com>
2021-04-19 00:33:11 +0200

    edit B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -75,7 +75,7 @@ public class GT4500 implements SpaceShip {
           }
         }
         break;
-        //új sor B-hez
+        //új sor B
 
       case ALL:
         // try to fire both of the torpedo stores
